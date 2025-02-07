acb463876c651624a6f7f3d338eeaf0ea730e412
Karim Serbouty <kserbouty@outlook.com>
2025-02-07 18:10:44 +0100

    Add template for static arrays

---
+++ src/array/static-array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * Static Array:
+ *
+ * Contiguous area of memory
+ * consisting of equal-size elements indexed
+ * by contiguous integers.
+ *
+ * Constant-time access to any element.
+ *
+ * Constant-time to add/remove at the end.
+ *
+ * Linear time to add/remove at an
+ * arbitrary location.
+ *
+ *
+ * Runtimes:
+ *
+ * Insert at the beginning [0]: O(n)
+ * Remove from the beginning [0]: O(n)
+ *
+ * Insert at the end [n-1]: O(1)
+ * Remove from the end [n-1]: O(1)
+ *
+ * Insert in the middle [n]: O(n)
+ * Remove from the middle [n]: O(n)
+ *
+ */
+int main(void)
+{
+    /* ... */
+}
